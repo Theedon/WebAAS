1c47e4b43d3901e02636ced1042faa941eefcc6b
Theedon <faniyantoyin@gmail.com>
2023-12-29 15:43:38 +0100

    feat: add theme colors and font variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import { Poppins, Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+export const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "WebAAS",
@@ -19,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("", poppins.className)}>{children}</body>
     </html>
   );
 }
