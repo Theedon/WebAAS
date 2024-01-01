b3f922ab0fbde5c66daece601e001905cccb544e
Theedon <faniyantoyin@gmail.com>
2024-01-01 12:02:35 +0100

    feat: add cors config for allowing proper origins

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,13 +1,3 @@
-/**
- * Configures and initializes an Apollo GraphQL server instance using the provided typeDefs and
- * resolvers. Creates a Next.js API route handler that will handle incoming GraphQL requests.
- *
- * The schema is created by combining the typeDefs and resolvers. An ApolloServer instance is
- * created with this schema. The startServerAndCreateNextHandler utility from @as-integrations/next
- * is used to generate a Next.js API route handler function that can handle GraphQL requests.
- *
- * This handler is then exported as both GET and POST handlers for the route.
- */
 import { ApolloServer } from "@apollo/server";
 import { typeDefs } from "@/backend/graphql/typeDefs";
 import { resolvers } from "@/backend/graphql/resolvers";
