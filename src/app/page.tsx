e83cd8d61ec7d804fc5a91913261b9eb33616dff
Theedon <faniyantoyin@gmail.com>
2024-01-01 01:25:16 +0100

    feat: generate schema and query types with codegen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 import Image from "next/image";
 
-export default function Home() {
+import { getClient } from "@/lib/apollo-clients/RSCClient";
+import { gql } from "@apollo/client";
+
+const query = gql`
+  query AllQuestions {
+    allQuestions {
+      id
+      option_a
+      option_b
+      option_c
+      option_d
+      question
+      subject_id
+    }
+  }
+`;
+
+export default async function Home() {
+  // const { data, error } = await getClient().query<>({ query });
+  // console.log(JSON.stringify(data));
   return <main className="bg-background">WebAAS</main>;
 }
