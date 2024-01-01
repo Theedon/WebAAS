0cb873dd38c8e146c614d3ee302be03ce2eb25ef
Theedon <faniyantoyin@gmail.com>
2024-01-01 12:02:35 +0100

    feat: add cors config for allowing proper origins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: process.env.NEXTAUTH_URL ?? "http://localhost:3000",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
