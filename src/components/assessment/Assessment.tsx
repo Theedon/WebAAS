3923c7e50ee829a9bdefdde5ec60c79811528723
Theedon <faniyantoyin@gmail.com>
2024-01-19 14:46:55 +0100

    refactor: remove hardcoded radioitems and map instead

--- src/components/assessment/Assessment.tsx
+++ src/components/assessment/Assessment.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { QuestionType } from "@/app/assessment/page";
-import ButtonSection from "./navigation/ButtonSection";
 import AssesmentNavigation from "./navigation";
 
 type AssessmentProps = {
@@ -17,76 +16,54 @@ type OptionsProp = {
 };
 function Assessment({ questionsData }: AssessmentProps) {
   const [currentQuestionId, setCurrentQuestionId] = useState<number>(0);
-  let optionData: OptionsProp[] = questionsData.map((question) => {
+
+  const editQuestionsData = questionsData.map((question, index) => {
     return {
-      id: question.id,
-      chosenOption: question.correct_option,
-      correctAnswer: question.correct_option,
+      index,
+      questionId: question.id,
+      choice: "",
+      correctChoice: question.correct_option,
     };
   });
-  const [option, setOption] = useState<OptionsProp[]>(optionData);
-  const setOptionData = (id: number, newOption: string) => {
-    setOption((prevOption) =>
-      prevOption.map((opt) =>
-        opt.id === id ? { ...opt, chosenOption: newOption } : opt,
-      ),
-    );
-  };
 
-  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const chosenOption = event.target.value;
-    setOptionData(currentQuestionId, chosenOption);
-    alert("changed");
-  };
+  const [options, setOptions] = useState<
+    {
+      index: number;
+      questionId: number;
+      choice: string;
+      correctChoice: string;
+    }[]
+  >(editQuestionsData);
 
   return (
     <div className="flex flex-col gap-10">
       <h2 className="text-xl font-semibold">
-        Question {questionsData[currentQuestionId].id}
+        Question {questionsData[currentQuestionId]?.id}
       </h2>
 
       <div className="flex flex-col">
-        <p>{questionsData[currentQuestionId].questionBody}</p>
+        <p>{questionsData[currentQuestionId]?.questionBody}</p>
         <RadioGroup
-          value={option[currentQuestionId].chosenOption}
+          value={
+            options.find((opt) => opt.questionId === currentQuestionId)?.choice
+          }
           name={`radioGroup${currentQuestionId}`}
+          onValueChange={(newValue: string) => {}}
         >
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem
-              value={questionsData[currentQuestionId].option_a}
-              id="A"
-            />
-            <Label htmlFor="A">
-              {questionsData[currentQuestionId].option_a}
-            </Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem
-              value={questionsData[currentQuestionId].option_b}
-              id="B"
-            />
-            <Label htmlFor="B">
-              {questionsData[currentQuestionId].option_b}
-            </Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem
-              value={questionsData[currentQuestionId].option_c}
-              id="C"
-            />
-            <Label htmlFor="C">
-              {questionsData[currentQuestionId].option_c}
-            </Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem
-              value={questionsData[currentQuestionId].option_d}
-              id="D"
-            />
-            <Label htmlFor="D">
-              {questionsData[currentQuestionId].option_d}
-            </Label>
-          </div>
+          {[
+            questionsData[currentQuestionId]?.option_a,
+            questionsData[currentQuestionId]?.option_b,
+            questionsData[currentQuestionId]?.option_c,
+            questionsData[currentQuestionId]?.option_d,
+          ].map((option: string) => (
+            <div key={option} className="flex items-center space-x-2">
+              <RadioGroupItem
+                value={option}
+                id={`${currentQuestionId}_${option}`}
+              />
+              <Label htmlFor={`${currentQuestionId}_${option}`}>{option}</Label>
+            </div>
+          ))}
         </RadioGroup>
       </div>
       <AssesmentNavigation
@@ -94,9 +71,6 @@ function Assessment({ questionsData }: AssessmentProps) {
         questionsData={questionsData}
         setCurrentQuestionId={setCurrentQuestionId}
       />
-      <button onClick={() => console.table(option)}>
-        HELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
-      </button>
     </div>
   );
 }
