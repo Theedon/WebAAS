ef12e5bb36a2457b01905e6789d5db4ee4f553d9
Theedon <Theedon@users.noreply.github.com>
2024-01-21 23:08:59 +0000

    apply formatting changes with prettier workflow

--- src/app/(auth-pages)/signup/__generated__/page.generated.tsx
+++ src/app/(auth-pages)/signup/__generated__/page.generated.tsx
@@ -1,12 +1,14 @@
-import * as Types from '../../../types';
+import * as Types from "../../../types";
 
 export type RegisterUserMutationVariables = Types.Exact<{
-  firstName: Types.Scalars['String']['input'];
-  lastName: Types.Scalars['String']['input'];
-  email: Types.Scalars['String']['input'];
-  password: Types.Scalars['String']['input'];
-  faculty: Types.Scalars['String']['input'];
+  firstName: Types.Scalars["String"]["input"];
+  lastName: Types.Scalars["String"]["input"];
+  email: Types.Scalars["String"]["input"];
+  password: Types.Scalars["String"]["input"];
+  faculty: Types.Scalars["String"]["input"];
 }>;
 
-
-export type RegisterUserMutation = { __typename?: 'Mutation', createUser: string };
+export type RegisterUserMutation = {
+  __typename?: "Mutation";
+  createUser: string;
+};
