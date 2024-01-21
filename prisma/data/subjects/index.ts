a1ebf510c4db6778084fdd852ed758e1e248657a
Theedon <faniyantoyin@gmail.com>
2024-01-21 12:02:14 +0100

    refactor: add enum for subjects data and modify seed

--- prisma/data/subjects/index.ts
+++ prisma/data/subjects/index.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { FacultyCode, SubjectType } from "./utils";
+import { SubjectCode } from "../questions/utils";
 
 const prisma = new PrismaClient();
-
-type SubjectsType = {
-  name: string;
-  description: string;
-  facultyId: string;
-  facultyCode: string;
-};
 
 const getFacultyIdByCode = async (code: string) => {
   const faculty = await prisma.faculty.findUnique({
@@ -15,14 +10,14 @@ const getFacultyIdByCode = async (code: string) => {
       code,
     },
   });
-  return faculty?.id ?? ""; // Assuming 'id' is the field you want to assign to facultyId
+  return faculty?.id ?? "";
 };
 
 const fillSubjectsWithId = async () => {
-  const Subjects: SubjectsType[] = await Promise.all(
+  const Subjects: SubjectType[] = await Promise.all(
     RawSubjects.map(async (subject) => ({
       ...subject,
-      facultyId: await getFacultyIdByCode(subject.facultyCode), // Assuming subject name can be used as a code
+      facultyId: await getFacultyIdByCode(subject.facultyCode),
     })),
   );
   return Subjects;
@@ -33,26 +28,43 @@ export const getSubjects = async () => {
   return subjects;
 };
 
-const RawSubjects: SubjectsType[] = [
+/**
+ * Subjects Data
+ **/
+const RawSubjects: SubjectType[] = [
   {
     name: "Physics",
+    code: SubjectCode.PHYSICS,
     description:
-      "It seeks to understand the fundamental laws and principles that govern everything from the tiniest subatomic particles to the vast expanses of galaxies. ",
+      "It seeks to understand the fundamental laws and principles that govern everything from the tiniest subatomic particles to the vast expanses of galaxies.",
     facultyId: "",
-    facultyCode: "SCI",
+    facultyCode: FacultyCode.SCIENCES,
   },
   {
     name: "Commerce",
+    code: SubjectCode.COMMERCE,
+
     description:
       "Dive into the dynamic world of business and trade! Commerce ignites your entrepreneurial spirit, exploring markets, finance, management, marketing, and global economics. Discover how businesses thrive, make strategic decisions, and navigate the exciting challenges of the commercial landscape.",
     facultyId: "",
-    facultyCode: "COM",
+    facultyCode: FacultyCode.COMMERCIAL,
   },
   {
     name: "French",
+    code: SubjectCode.FRENCH,
+
     description:
       "Bonjour, francophiles! Embark on a linguistic and cultural journey with French. Master the language of love, art, and diplomacy. Unlock captivating literature, engage in lively conversations, and explore the rich history and vibrant cultures of France and Francophone communities around the globe",
     facultyId: "",
-    facultyCode: "ART",
+    facultyCode: FacultyCode.ARTS,
+  },
+  {
+    name: "Accounting",
+    code: SubjectCode.ACCOUNTING,
+
+    description:
+      "Explore the principles of financial accounting, including recording financial transactions, preparing financial statements, and understanding financial performance.",
+    facultyId: "",
+    facultyCode: FacultyCode.COMMERCIAL,
   },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,17 +5,18 @@ import { getSubjects } from "./data/subjects";
 const prisma = new PrismaClient();
 
 async function main() {
+  //insert faculties data
   for (let faculty of Faculties) {
     await prisma.faculty.create({
       data: faculty,
     });
   }
 
+  //insert subjects data
   const Subjects = await getSubjects();
   console.log(JSON.stringify(Subjects[1]));
   for (let subject of Subjects) {
     const { facultyId, facultyCode, ...filteredSubject } = subject;
-
     await prisma.subject.create({
       data: {
         ...filteredSubject,
