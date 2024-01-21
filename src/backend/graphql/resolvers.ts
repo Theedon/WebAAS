14dcd67cea9f42dbdcd3722fab1ab779b125f601
Theedon <faniyantoyin@gmail.com>
2024-01-21 21:55:06 +0100

    feat: add typedefs and resolvers for getting test questions

--- src/backend/graphql/resolvers.ts
+++ src/backend/graphql/resolvers.ts
@@ -2,11 +2,13 @@ import { merge } from "lodash";
 import { resolvers as SubjectResolvers } from "./types/Subjects";
 import { resolvers as QuestionResolvers } from "./types/Questions";
 import { resolvers as CreateUserResolvers } from "./mutations/CreateUser";
+import { resolvers as TestQuestionResolvers } from "./types/TestQuestions";
 
 const mergedResolvers = [
   SubjectResolvers,
   QuestionResolvers,
   CreateUserResolvers,
+  TestQuestionResolvers,
 ];
 
 export const resolvers = merge({}, ...mergedResolvers);
