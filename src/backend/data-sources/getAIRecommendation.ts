c032dc8c7c4b46515ff2e0a2e01bf2a33fd79bfd
Theedon <faniyantoyin@gmail.com>
2024-01-27 20:11:43 +0100

    feat: add mutation to save exam data and get recommendation

---
+++ src/backend/data-sources/getAIRecommendation.ts
@@ -0,0 +1,83 @@
+import { QuestionType } from "@/components/assessment/Assessment";
+import { PrismaClient } from "@prisma/client";
+import OpenAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const prisma = new PrismaClient();
+
+async function updateSubjectNames(
+  questionsArray: QuestionType[],
+): Promise<QuestionType[]> {
+  const updatedQuestions: QuestionType[] = [];
+  for (const question of questionsArray) {
+    const { subject_id, ...rest } = question;
+    try {
+      const subject = await prisma.subject.findUnique({
+        where: {
+          id: subject_id,
+        },
+      });
+      if (subject) {
+        const updatedQuestion: QuestionType = {
+          ...rest,
+          subject_id: subject.name,
+        };
+        updatedQuestions.push(updatedQuestion);
+      } else {
+        console.error(`Subject not found for subject_id: ${subject_id}`);
+      }
+    } catch (error) {
+      console.error("Error fetching subject:", error);
+    }
+  }
+
+  return updatedQuestions;
+}
+
+const constructString = (assesmentInfo: QuestionType[]): string => {
+  const constructArray = assesmentInfo.map((exam) => {
+    return `In the subject ${exam.subject_id}, to ${
+      exam.question
+    }, the student's answer was ${
+      exam.choice || "nil"
+    }, the correct answer is ${exam.correct_option}.`;
+  });
+
+  return constructArray.join("; ");
+};
+
+export const getAIRecommendations = async (assesmentInfo: QuestionType[]) => {
+  const assesmentWithSubjectNames = await updateSubjectNames(assesmentInfo);
+  const questionStringArray = constructString(assesmentWithSubjectNames);
+
+  const prompt = `
+  As an esteemed academic advisor, students seek your expertise in navigating the crucial decision of choosing a college track post-high school. Your mission is to dissect their performance across five subjects, exposing strengths and weaknesses, and offering blunt insights into courses that resonate with their abilities. End your analysis by unapologetically recommending the top three college courses tailored to their strengths. Remember, you're addressing the student directly, so be brutally honest and opinionated, Give as much information as you can.
+
+  Here are the test results for this user:
+  "${questionStringArray}"  
+  `;
+
+  const genAI = new GoogleGenerativeAI(
+    process.env.GOOGLE_GENERATIVE_API_KEY as string,
+  );
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  // console.log("this is the recommendation", text);
+
+  const updateUser = await prisma.user.update({
+    where: {
+      id: "clrozkupz0087wh88ldkkw9ky",
+    },
+    data: {
+      ai_recommendation: Buffer.from(text),
+      onboarded: true,
+      test_information: Buffer.from(questionStringArray),
+    },
+  });
+
+  console.log("recommendation added for user successfully");
+
+  return text;
+};
