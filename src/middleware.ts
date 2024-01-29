1383db6465f9a8fc469409c819f53145c5bbfe05
Theedon <faniyantoyin@gmail.com>
2024-01-29 13:06:52 +0100

    feat: make graphql route public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { createContext } from "./backend/graphql/context";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/graphql"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
