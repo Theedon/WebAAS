e3553382451885c5f5a8ab5531fa164b8c37a531
Theedon <faniyantoyin@gmail.com>
2024-01-29 15:48:32 +0100

    feat: add data-source function to get user recommendation from db

---
+++ src/backend/data-sources/getAdvice.ts
@@ -0,0 +1,13 @@
+import prisma from "../prisma/prisma";
+
+export const getAdvice = async (userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  const ai_recommendation = user?.ai_recommendation?.toString();
+
+  return { userId, ai_recommendation };
+};
