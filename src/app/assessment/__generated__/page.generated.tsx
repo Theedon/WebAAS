2d10d5cee8739d360eb9c431531abed73894a6c5
Theedon <Theedon@users.noreply.github.com>
2024-01-31 18:13:16 +0000

    apply formatting changes with prettier workflow

--- src/app/assessment/__generated__/page.generated.tsx
+++ src/app/assessment/__generated__/page.generated.tsx
@@ -1,8 +1,22 @@
-import * as Types from '../../types';
+import * as Types from "../../types";
 
 export type TestQuestionsQueryVariables = Types.Exact<{
-  faculty: Types.Scalars['String']['input'];
+  faculty: Types.Scalars["String"]["input"];
 }>;
 
-
-export type TestQuestionsQuery = { __typename?: 'Query', testQuestions: Array<Array<{ __typename?: 'Question', id: string, option_a: string | null, option_b: string | null, option_c: string | null, option_d: string | null, subject_id: string | null, question: string, correct_option: string | null }>> };
+export type TestQuestionsQuery = {
+  __typename?: "Query";
+  testQuestions: Array<
+    Array<{
+      __typename?: "Question";
+      id: string;
+      option_a: string | null;
+      option_b: string | null;
+      option_c: string | null;
+      option_d: string | null;
+      subject_id: string | null;
+      question: string;
+      correct_option: string | null;
+    }>
+  >;
+};
