cf32902840ce2ed459264f27fc5bae8c5d45d115
Theedon <Theedon@users.noreply.github.com>
2024-02-19 09:48:07 +0000

    apply formatting changes with prettier workflow

--- src/backend/data-sources/getFacultyFromId.ts
+++ src/backend/data-sources/getFacultyFromId.ts
@@ -2,7 +2,6 @@ import { FacultyDomain } from "../domains/FacultyDomain";
 import prisma from "../prisma/prisma";
 
 export const getFacultyFromId = async (facultyId: string) => {
-
   const faculty = await prisma.faculty.findUnique({
     where: {
       id: facultyId,
