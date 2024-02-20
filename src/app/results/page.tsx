208da68608312d5418a54b78b1b074ad8862bb87
Theedon <faniyantoyin@gmail.com>
2024-02-20 18:13:40 +0100

    feat: run AI functions from when user gets into results page

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,6 +1,6 @@
 import { getClient } from "@/lib/apollo-clients/RSCClient";
 import gql from "graphql-tag";
-import React from "react";
+import React, { Suspense } from "react";
 import getCurrentUserId from "@/lib/globalUserContext";
 import Markdown from "react-markdown";
 import { redirect } from "next/navigation";
@@ -9,12 +9,17 @@ import {
   UserAdviceQueryVariables,
 } from "./__generated__/page.generated";
 import { stripData } from "@/lib/utils";
+import { getUserCoursesAI } from "@/backend/data-sources/getUserCoursesAI";
+import { getAIRecommendation } from "@/backend/data-sources/getAIRecommendation";
+import LoadingPage from "../loading";
 
 const GET_RECOMMENDATION = gql`
   query UserAdvice($userId: String!) {
     user(id: $userId) {
       userExamInfo {
+        test_information
         ai_recommendation
+        rec_course_1
       }
     }
   }
@@ -29,16 +34,31 @@ async function ResultPage() {
     variables: { userId: getCurrentUserId() as string },
   });
 
-  if (!data.user.userExamInfo.ai_recommendation) redirect("/assessment");
+  const userId = getCurrentUserId() as string;
+  if (!data.user.userExamInfo.test_information) {
+    redirect("/assessment");
+  } else if (!data.user.userExamInfo.ai_recommendation) {
+    await getAIRecommendation(userId);
+    console.log("done");
+    await getUserCoursesAI(userId);
+    console.log("second done");
+    redirect("results");
+  }
 
   return (
     <div>
       {data.user.userExamInfo.ai_recommendation ? (
-        <Markdown>
-          {stripData(data.user.userExamInfo.ai_recommendation)}
-        </Markdown>
+        <div>
+          {data.user.userExamInfo.ai_recommendation ? (
+            <Markdown>
+              {stripData(data.user.userExamInfo.ai_recommendation)}
+            </Markdown>
+          ) : (
+            <p>No data</p>
+          )}
+        </div>
       ) : (
-        <p>No data</p>
+        <LoadingPage />
       )}
     </div>
   );
