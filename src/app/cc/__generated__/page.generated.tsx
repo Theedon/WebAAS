c7450f5e4e354f6b9d6731cff17df149cea39e33
Theedon <Theedon@users.noreply.github.com>
2024-02-20 09:01:05 +0000

    chore(prettier): beautify code

--- src/app/cc/__generated__/page.generated.tsx
+++ src/app/cc/__generated__/page.generated.tsx
@@ -1,6 +1,17 @@
-import * as Types from '../../types';
+import * as Types from "../../types";
 
-export type AllQuestionsQueryVariables = Types.Exact<{ [key: string]: never; }>;
+export type AllQuestionsQueryVariables = Types.Exact<{ [key: string]: never }>;
 
-
-export type AllQuestionsQuery = { __typename?: 'Query', allQuestions: Array<{ __typename?: 'Question', id: string, option_a: string | null, option_b: string | null, option_c: string | null, option_d: string | null, question: string, subject_id: string | null }> };
+export type AllQuestionsQuery = {
+  __typename?: "Query";
+  allQuestions: Array<{
+    __typename?: "Question";
+    id: string;
+    option_a: string | null;
+    option_b: string | null;
+    option_c: string | null;
+    option_d: string | null;
+    question: string;
+    subject_id: string | null;
+  }>;
+};
