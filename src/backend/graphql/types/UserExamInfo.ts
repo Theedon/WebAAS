e9f35bebdd6c6474dda582e03b5db3f966dd8e9e
Theedon <faniyantoyin@gmail.com>
2024-02-20 16:18:26 +0100

    fix: make some user info fields nullable

--- src/backend/graphql/types/UserExamInfo.ts
+++ src/backend/graphql/types/UserExamInfo.ts
@@ -2,12 +2,12 @@ import gql from "graphql-tag";
 
 export const typeDefs = gql`
   type UserExamInfo {
-    test_information: String!
-    ai_recommendation: String!
-    rec_course_1: String!
+    test_information: String
+    ai_recommendation: String
+    rec_course_1: String
     rec_course_2: String
     rec_course_3: String
-    anti_course_1: String!
+    anti_course_1: String
     anti_course_2: String
     anti_course_3: String
     taken_exam: Boolean!
