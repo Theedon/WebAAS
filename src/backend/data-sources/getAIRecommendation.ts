7d50f1a0dfef9d6b5616ab04c8566ad970b36fd9
Theedon <faniyantoyin@gmail.com>
2024-02-21 14:21:46 +0100

    feat: update AI prompt for recommendation

--- src/backend/data-sources/getAIRecommendation.ts
+++ src/backend/data-sources/getAIRecommendation.ts
@@ -2,12 +2,6 @@ import prisma from "../prisma/prisma";
 import { promptGoogleAI } from "./promptGoogleAI";
 
 export const getAIRecommendation = async (userId: string) => {
-  const userNameObj = await prisma.user.findFirst({
-    where: {
-      clerk_id: userId,
-    },
-    select: { first_name: true },
-  });
   const userInfo = await prisma.userToExam.findUnique({
     where: { clerk_id: userId },
     select: {
@@ -17,13 +11,12 @@ export const getAIRecommendation = async (userId: string) => {
   const questionStringArray = userInfo.test_information.toString();
 
   const fullRecommendationPrompt = `
-    You are an esteemed academic advisor in the Nigerian University system, and i am a student called ${userNameObj.first_name} who needs your assistance in navigating a crucial decision: choosing a university course post-secondary school for a Nigerian University. I will provide you with their test results for analysis across five subjects, revealing strengths and weaknesses in details. Your mission is to offer insights into courses that align with my abilities and recommend the top three college courses tailored to my strengths. Remember, you're addressing me directly, so be honest and as helpful as possible. Please provide as much information as you can. Ensure your response is in pure markdown.
+  You are an esteemed academic advisor in the Nigerian University system,and i am a student who needs your assistance in choosing a university course post-high school. I will provide you with my test results in JSON for analysis across five subjects. Your mission is to offer insights into courses that align with my abilities. Ensure your response is in pure markdown.
   
-    Here are my test results: "${questionStringArray}"
-    
-    Please organize your response into three bold headings: Analysis of Test Results, Recommended Courses, Least Suitable, Additional Considerations. Add spaces after each heading and make sure that the headings are centered on their lines. Ensure to refer to the student by name and also ensure that you return exactly three recommended courses and three least suitable courses
-    "${questionStringArray}"    
-    `;
+  Here are my test results: "${questionStringArray}"
+  
+  Please organize your response into four bold headings: Analysis of Test Results, Recommended Courses, Least Suitable, Additional Considerations. Add spaces after each heading and make sure that the headings are centered on their lines. Ensure that you return exactly three recommended courses and three least suitable courses. I need you to be as descriptive as possible
+  `;
   const fullRecommendation = await promptGoogleAI(fullRecommendationPrompt);
 
   const updateUser = await prisma.userToExam.update({
