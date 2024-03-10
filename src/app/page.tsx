7e433b24605f4c9c6b19a79012b327859609d911
Theedon <faniyantoyin@gmail.com>
2024-03-10 15:34:48 +0100

    feat: add dashboard ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,107 @@
-import { checkIfUserRegistered } from "@/backend/data-sources/checkIfUserRegistered";
+import SubjectCard from "@/components/dashboard/SubjectCard";
+import { getClient } from "@/lib/apollo-clients/RSCClient";
 import getCurrentUserId from "@/lib/globalUserContext";
 import { gql } from "@apollo/client";
-import { redirect } from "next/navigation";
+import {
+  GetSubjectsQuery,
+  GetSubjectsQueryVariables,
+} from "./__generated__/page.generated";
+import { dateWrangler, getCourseFromFaculty } from "@/lib/utils";
+import { AdvisorsDataType, columns } from "@/components/advisors/Columns";
+import { DataTable } from "@/components/advisors/DataTable";
 
 const query = gql`
-  query AllQuestions {
-    allQuestions {
+  query GetSubjects($userId: String!) {
+    user(id: $userId) {
+      userExamInfo {
+        rec_course_1
+        rec_course_2
+        rec_course_3
+        anti_course_1
+        anti_course_2
+        anti_course_3
+      }
+    }
+    allAdvisors {
       id
-      option_a
-      option_b
-      option_c
-      option_d
-      question
-      subject_id
+      firstName
+      lastName
+      email
+      created_at
+      updated_at
+      faculty {
+        name
+      }
+      phone_no
     }
   }
 `;
 
-export default function Home() {
-  return <main>Dashboard</main>;
+export default async function Home() {
+  const { data, error } = await getClient().query<
+    GetSubjectsQuery,
+    GetSubjectsQueryVariables
+  >({
+    query,
+    variables: { userId: getCurrentUserId() as string },
+  });
+  const wrangledData: AdvisorsDataType[] = data.allAdvisors.map((user) => {
+    return {
+      id: user.id,
+      first_name: user.firstName,
+      last_name: user.lastName,
+      email: user.email,
+      created_at: dateWrangler(user.created_at),
+      updated_at: dateWrangler(user.updated_at),
+      phone_no: user.phone_no ?? "",
+      faculty: user.faculty?.name ?? "",
+    };
+  });
+  return (
+    <main className="flex flex-col gap-5">
+      <SubjectSection
+        header="Recommended Subjects"
+        course_1={data.user.userExamInfo.rec_course_1 ?? ""}
+        course_2={data.user.userExamInfo.rec_course_2 ?? ""}
+        course_3={data.user.userExamInfo.rec_course_3 ?? ""}
+      />
+      <SubjectSection
+        header="Not Recommended"
+        course_1={data.user.userExamInfo.anti_course_1 ?? ""}
+        course_2={data.user.userExamInfo.anti_course_2 ?? ""}
+        course_3={data.user.userExamInfo.anti_course_3 ?? ""}
+      />
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={wrangledData} />
+      </div>
+    </main>
+  );
 }
+
+type SubjectSectionProps = {
+  course_1: string;
+  course_2: string;
+  course_3: string;
+  header: string;
+};
+const SubjectSection = ({
+  course_1,
+  course_2,
+  course_3,
+  header,
+}: SubjectSectionProps) => {
+  return (
+    <div>
+      <h2 className="text-center text-lg text-primary">{header}</h2>
+      <section className="flex flex-col items-center justify-center gap-5 md:flex-row">
+        {[course_1, course_2, course_3].map((subject, index) => (
+          <SubjectCard
+            key={index}
+            course={getCourseFromFaculty(subject ?? "").course}
+            faculty={getCourseFromFaculty(subject ?? "").faculty}
+          />
+        ))}
+      </section>
+    </div>
+  );
+};
--- src/components/dashboard/SubjectCard.tsx
+++ src/components/dashboard/SubjectCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -10,32 +11,35 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { BookText } from "lucide-react";
 
 type SubjectCardProps = {
-  course: string;
-  faculty: string;
+  course: string | null;
+  faculty: string | null;
 };
 function SubjectCard({ course, faculty }: SubjectCardProps) {
   return (
-    <Card className="w-[350px]">
+    <Card className="w-[350px] bg-primary text-xs md:text-sm">
       <CardHeader className="flex items-center justify-center">
-        <CardTitle> Recommendation</CardTitle>
+        <CardTitle className="text-lg">
+          <BookText />
+        </CardTitle>
         {/* <CardDescription>Recommendation</CardDescription> */}
       </CardHeader>
       <CardContent>
         <div className="grid w-full items-center gap-4">
           <div className="flex flex-col space-y-1.5">
             <Label htmlFor="name">Course</Label>
-            <div className="rounded-lg border p-3">{course}</div>
+            <div className="rounded-lg border p-3">{course ?? "Nil"}</div>
           </div>
           <div className="flex flex-col space-y-1.5">
             <Label htmlFor="name">Faculty</Label>
-            <div className="rounded-lg border p-3 ">{faculty}</div>
+            <div className="rounded-lg border p-3 ">{faculty ?? "Nil"}</div>
           </div>
         </div>
       </CardContent>
       <CardFooter className="flex items-center justify-center">
-        <Button>See full Recommendation</Button>
+        <Button variant={"outline"}>See full Recommendation</Button>
       </CardFooter>
     </Card>
   );
