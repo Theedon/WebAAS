cb40d36154f369dc9ec8a1f048898fb38755d56b
Theedon <faniyantoyin@gmail.com>
2024-03-11 12:15:50 +0100

    feat: add onClick to navigate to recommendations from dashboard cards

--- src/components/dashboard/SubjectCard.tsx
+++ src/components/dashboard/SubjectCard.tsx
@@ -12,12 +12,14 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { BookText } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 type SubjectCardProps = {
   course: string | null;
   faculty: string | null;
 };
 function SubjectCard({ course, faculty }: SubjectCardProps) {
+  const router = useRouter();
   return (
     <Card className="w-[350px] bg-primary text-xs md:text-sm">
       <CardHeader className="flex items-center justify-center">
@@ -43,7 +45,14 @@ function SubjectCard({ course, faculty }: SubjectCardProps) {
         </div>
       </CardContent>
       <CardFooter className="flex items-center justify-center">
-        <Button variant={"outline"}>See full Recommendation</Button>
+        <Button
+          onClick={() => {
+            router.push("/results");
+          }}
+          variant={"outline"}
+        >
+          See full Recommendation
+        </Button>
       </CardFooter>
     </Card>
   );
