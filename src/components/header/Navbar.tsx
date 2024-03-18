21033b5c14e65a50b05593f7a9090e09fc77e894
Theedon <faniyantoyin@gmail.com>
2024-03-18 19:42:32 +0100

    fix: ensure fallback for no user is present in navbar

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -29,6 +29,7 @@ async function Navbar() {
     >({
       query,
       variables: { userId: getCurrentUserId() as string },
+      fetchPolicy: "no-cache",
     });
 
     return (
@@ -61,6 +62,29 @@ async function Navbar() {
     );
   } catch (error: unknown) {
     console.log(error);
+    return (
+      <header className="fixed left-0 top-0 z-10 my-1 w-full max-w-full border-b-2 bg-opacity-30 shadow-md backdrop-blur-md">
+        <div className="flex h-[10vh] items-center justify-between rounded-b-xl bg-primary px-7 md:px-10 dark:text-background">
+          <Image src={app_logo} alt="app logo" className="size-14"></Image>
+
+          <div className="hidden md:flex">
+            <NavButtons href="/">Dashboard</NavButtons>
+            <NavButtons href="/results">Results</NavButtons>
+
+            <NavButtons href="/schedule">class schedule</NavButtons>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <ThemeToggle />
+
+            <Button variant={"ghost"} className="flex" size={"icon"}>
+              <UserButton afterSignOutUrl="/" />
+            </Button>
+            <NavDrawer NavItems={NavItems} />
+          </div>
+        </div>
+      </header>
+    );
   }
 }
 
